d7b0a7d2fc17af8914c4521c7e6137b8f00bf78f
Mask <2421697126@qq.com>
2019-03-19 20:29:49 +0800

    hystrix-dashboard
    10-7

--- client/src/main/java/com/mask/product/client/ProductClient.java
+++ client/src/main/java/com/mask/product/client/ProductClient.java
@@ -4,21 +4,34 @@ import com.mask.product.common.DecreaseStockInput;
 import com.mask.product.common.ProductInfoOutput;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-/**
- * Created by 廖师兄
- * 2017-12-10 21:04
- */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
-    @PostMapping("/product/listForOrder")
-    List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
+	@PostMapping("/product/listForOrder")
+	List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
 
-    @PostMapping("/product/decreaseStock")
-    void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+	@PostMapping("/product/decreaseStock")
+	void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+	@Component
+	static class ProductClientFallback implements ProductClient {
+
+		@Override
+		public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
 }
